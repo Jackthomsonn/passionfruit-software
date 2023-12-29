0f5b408dca40646459759688b4133df56ac942f0
Jack Thomson <jack.thomson@sero.life>
2023-12-29 11:39:09 +0000

    upate

--- apps/passionfruit-software/components/main-nav.tsx
+++ apps/passionfruit-software/components/main-nav.tsx
@@ -6,7 +6,7 @@ import { siteConfig } from "@/config/site";
 export function MainNav() {
 	return (
 		<div className="flex justify-between w-screen">
-			<Link href="/" className="flex items-center">
+			<Link href="#" className="flex items-center">
 				<Icons.logo className="h-6 w-6" />
 				<span className="inline-block font-bold text-white">{siteConfig.name}</span>
 			</Link>
